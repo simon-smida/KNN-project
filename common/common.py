58fdb297bc0116982bca890ed7af7cce2629f214
Milan Tichavský <milantichavsky@seznam.cz>
2024-04-29 13:20:53 +0200

    Change the way Voxceleb1 is being downloaded

---
+++ common/common.py
@@ -0,0 +1,13 @@
+import logging
+import os
+from pathlib import Path
+from torchaudio.datasets import VoxCeleb1Identification
+
+DATASET_DIR = Path(os.getenv("KNN_DATASET_DIR", default="voxceleb1"))
+DATASET_DIR.mkdir(parents=True, exist_ok=True)
+
+
+def download_voxceleb(split="train"):
+    logger = logging.getLogger(__name__)
+    logger.info(f"Downloading VoxCeleb1 into {DATASET_DIR}...")
+    VoxCeleb1Identification(root=DATASET_DIR, subset=split, download=True)
