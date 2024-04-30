21991b7c38fdfeb50a8c9a39a28b0237a9f88e5e
Milan Tichavský <milantichavsky@seznam.cz>
2024-04-30 08:28:02 +0200

    Enable loading of a model from a checkpoint

--- train_ecapa.py
+++ train_ecapa.py
@@ -13,10 +13,11 @@ from speechbrain.processing.features import InputNormalization
 from speechbrain.nnet.losses import LogSoftmaxWrapper, AdditiveAngularMargin
 
 BATCH_SIZE = int(os.getenv("KNN_BATCH_SIZE", default=4))
-MODEL_DIR = Path(os.getenv("KNN_MODEL_DIR", default="experiments/models"))
-MODEL_DIR.mkdir(parents=True, exist_ok=True)
+MODEL_OUT_DIR = Path(os.getenv("KNN_MODEL_OUT_DIR", default="experiments/models"))
+MODEL_OUT_DIR.mkdir(parents=True, exist_ok=True)
 DEBUG = True if os.getenv("KNN_DEBUG", default="False") == "True" else False
-VOXCELEB_UTTERANCES = 150_000
+MODEL_IN_DIR = os.getenv("KNN_MODEL_IN_DIR", default=None)
+
 
 VIEW_STEP = 50
 
@@ -44,7 +45,6 @@ def get_spectrum_feats(wavs: torch.Tensor, legths: torch.Tensor):
 
 
 if __name__ == "__main__":
-    print(f"Starting training with batch size {BATCH_SIZE}...")
     # A bool that, if True, causes cuDNN to benchmark multiple convolution algorithms and select the fastest
     # However, input tensor of the model has to have constant length and the model cannot change (i.s., it doesn't have
     # layers that are only activated on certain conditions or different number of times in every run).
@@ -56,13 +56,23 @@ if __name__ == "__main__":
     train_dataloader = DataLoader(voxceleb1, batch_size=BATCH_SIZE, shuffle=True, collate_fn=collate_with_padding)
 
     model = ECAPA_TDNN(input_size=80, lin_neurons=192, device=device_str)
+    classify = Classifier(input_size=192, lin_neurons=192, out_neurons=1252)
+
+    if MODEL_IN_DIR is not None:
+        MODEL_IN_DIR = Path(MODEL_IN_DIR)
+        print(f"Loading models from {MODEL_IN_DIR}...")
+        model.load_state_dict(torch.load(MODEL_IN_DIR / "ecapa_tdnn.state_dict"))
+        classify.load_state_dict(torch.load(MODEL_IN_DIR / "classifier.state_dict"))
+
     model.to(device)
     model.train()
-    classify = Classifier(input_size=192, lin_neurons=192, out_neurons=1252)
     classify.to(device)
     classify.train()
 
     optimizer = torch.optim.Adam(list(model.parameters()) + list(classify.parameters()), lr=0.001, weight_decay=0.000002)
+    if MODEL_IN_DIR is not None:
+        optimizer.load_state_dict(torch.load(MODEL_IN_DIR / "optimizer.state_dict"))
+
     criterion = LogSoftmaxWrapper(AdditiveAngularMargin(margin=0.2, scale=30))
     fbank = Fbank(n_mels=80, left_frames=0, right_frames=0, deltas=False)
     mean_var_norm = InputNormalization(norm_type="sentence", std_norm=False)
@@ -70,6 +80,7 @@ if __name__ == "__main__":
     iteration = 0
     loss_acc = 0
     hits_acc = 0
+    print(f"Starting training with batch size {BATCH_SIZE}...")
     for batch, lengths in train_dataloader:
         iteration += 1
 
@@ -104,10 +115,7 @@ if __name__ == "__main__":
         # Stop training after x iterations
         if DEBUG is True and iteration == 300:
             break
-        if iteration == int(VOXCELEB_UTTERANCES / BATCH_SIZE):
-            print("The iterator seems to be infinite, quitting after ~1 epoch.")
-            break  # This condition can be deleted, if iterator iterates over the dataset only once
 
-    torch.save(model.state_dict(), MODEL_DIR / "ecapa_tdnn.state_dict")
-    torch.save(classify.state_dict(), MODEL_DIR / "classifier.state_dict")
-    torch.save(optimizer.state_dict(), MODEL_DIR / "optimizer.state_dict")
+    torch.save(model.state_dict(), MODEL_OUT_DIR / "ecapa_tdnn.state_dict")
+    torch.save(classify.state_dict(), MODEL_OUT_DIR / "classifier.state_dict")
+    torch.save(optimizer.state_dict(), MODEL_OUT_DIR / "optimizer.state_dict")
